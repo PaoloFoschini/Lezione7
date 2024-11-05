edaeec13a41fb42b431cb611bb6316437586ddfa
Paolo Foschini <paolo.foschini2@studio.unibo.it>
2024-11-05 16:49:42 +0100

    73 almost finished

--- 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -5,6 +5,7 @@ import it.unibo.bank.api.BankAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
@@ -21,7 +22,9 @@ class TestStrictBankAccount {
      */
     @BeforeEach
     public void setUp() {
-        fail("To be implemented");
+        this.mRossi = new AccountHolder("Mario", "Rossi", 1);
+        this.bankAccount = new StrictBankAccount(mRossi, 0.0);
+        //fail("To be implemented");
     }
 
     /**
@@ -29,7 +32,10 @@ class TestStrictBankAccount {
      */
     @Test
     public void testInitialization() {
-        fail("To be implemented");
+        assertEquals(0.0,bankAccount.getBalance());
+        assertEquals(0,bankAccount.getTransactionsCount());
+        assertEquals(mRossi, bankAccount.getAccountHolder());
+        //fail("To be implemented");
     }
 
     /**
@@ -37,7 +43,10 @@ class TestStrictBankAccount {
      */
     @Test
     public void testManagementFees() {
-        fail("To be implemented");
+        bankAccount.deposit(1, 100);
+        bankAccount.chargeManagementFees(1);
+        assertEquals((100 - (5 + bankAccount.getTransactionsCount() * 0.1)), bankAccount.getBalance());
+        //fail("To be implemented");
     }
 
     /**
@@ -45,7 +54,8 @@ class TestStrictBankAccount {
      */
     @Test
     public void testNegativeWithdraw() {
-        fail("To be implemented");
+        assertEquals(new IllegalArgumentException(), bankAccount.withdraw(1, -100));
+        //fail("To be implemented");
     }
 
     /**
@@ -53,6 +63,9 @@ class TestStrictBankAccount {
      */
     @Test
     public void testWithdrawingTooMuch() {
-        fail("To be implemented");
+        bankAccount.withdraw(1, bankAccount.getBalance());
+        bankAccount.withdraw(1, 1024);
+        assertEquals(0, bankAccount.getBalance());
+        //fail("To be implemented");
     }
 }
